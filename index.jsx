83a3f32a0086ef054202d7d80adb269938c7d92b
rpidanny <abhishekmaharjan1993@gmail.com>
2019-01-09 01:32:03 +0100

    Add nodes to graph with styling

--- index.jsx
+++ index.jsx
@@ -1,19 +1,143 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
+import { select, event } from 'd3-selection'
+import {
+  forceLink,
+  forceSimulation,
+  forceCenter,
+  forceManyBody,
+  forceCollide
+} from 'd3-force'
+import { zoom } from 'd3-zoom'
+import Universe from './components/Universe'
 import './style.css'
 
 class NetworkGraph extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      nodes: [],
+      links: [],
+      simulation: null,
+      width: 100,
+      height: 100,
+      transform: {
+        x: 0,
+        y: 0,
+        k: 1
+      }
+    }
+    this.initSimulation = this.initSimulation.bind(this)
+    this.updateSimulation = this.updateSimulation.bind(this)
+    this.onTick = this.onTick.bind(this)
+    this.svg = React.createRef()
+    this.nodes = []
+    this.links = []
+  }
+
+  componentDidMount () {
+    this.initSimulation()
+    this.setState({
+      ...this.props,
+      width: this.svg.current.clientWidth,
+      height: this.svg.current.clientHeight
+    }, () => {
+      this.updateSimulation()
+    })
+  }
+
+  initSimulation () {
+    // const svg = select('.networkGraph')
+    const linkForce = forceLink()
+      .id(link => link.id)
+      .strength(link => link.strength)
+      .distance(link => link.distance)
+    const simulation = forceSimulation().force('link', linkForce)
+
+    // TODO: replace select dependency
+    // add zoom capabilities
+    const svg = select('.svgGraph')
+    const zoomHandler = zoom().on('zoom', () => {
+      this.setState({
+        transform: event.transform
+      })
+    })
+    zoomHandler(svg)
+
+    this.setState({
+      simulation
+    })
+  }
+
+  updateSimulation () {
+    const { nodes, links, simulation, width, height } = this.state
+    this.nodes = nodes
+    this.links = links
+    simulation
+      .nodes(this.nodes)
+      .on('tick', this.onTick)
+    // simulation.force('link').links(this.links)
+    simulation
+      .force('center', forceCenter(width / 2, height / 2))
+      .force('attraceForce', forceManyBody().strength(10))
+      .force('charge', forceManyBody().strength(-10))
+      .force(
+        'collision',
+        forceCollide()
+          .radius(node => 45)
+          .strength(0.7)
+      )
+
+    simulation
+      .alpha(0.4)
+      .alphaTarget(0)
+      .velocityDecay(0.4)
+      .restart()
+    // while (simulation.alpha() >= 0.02) {
+    //   simulation.tick()
+    // }
+  }
+
+  onTick (e) {
+    this.setState({
+      nodes: this.nodes
+    })
   }
 
   render () {
+    const { nodes, transform } = this.state
+
     return (
-      <div>
-        <svg className='networkGraph' width='100%' height='100%' />
+      <div
+        className='networkGraph'
+      >
+        <svg
+          className='svgGraph'
+          width='100%'
+          height='100%'
+          ref={this.svg}
+        >
+          <Universe
+            nodes={nodes}
+            transform={
+              `translate(${transform.x},${transform.y}) scale(${transform.k})`
+            }
+          />
+        </svg>
+        <span className='zoomIndicator'>{parseInt(transform.k * 100, 10)} %</span>
       </div>
     )
   }
+}
+
+NetworkGraph.defaultProps = {
+  nodes: [],
+  links: []
+}
+
+NetworkGraph.propTypes = {
+  nodes: PropTypes.array,
+  links: PropTypes.array
 }
 
 export default NetworkGraph
